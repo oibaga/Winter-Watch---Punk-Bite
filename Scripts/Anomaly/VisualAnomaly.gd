e7c64aa80621e713fe18f971c2b43fbdf2011913
CaioLeaoMarinho <caiolmo10@gmail.com>
2025-12-01 16:23:45 -0300

    Anomalia de sol ajustada, e porta banheiro

--- Scripts/Anomaly/VisualAnomaly.gd
+++ Scripts/Anomaly/VisualAnomaly.gd
@@ -1,10 +1,10 @@
 extends Anomaly
 
+@export var sunRef : Sprite3D
 @export var anomalies : Array[Node3D]
 var currentAnomaly : Node3D
-const SUN = preload("res://Shaders/SkyShaders/Sun.tres")
-const SUN_EYED = preload("res://Shaders/SkyShaders/SunEyed.tres")
-@export var world_environment: WorldEnvironment
+const SUN = preload("res://Assets/Materiais/Sun.png")
+const SUN_EYED = preload("res://Assets/Materiais/SunEyed.png")
 
 func _ready() -> void:
 	for anomaly in anomalies:
@@ -15,9 +15,10 @@ func SpawnAnomaly():
 
 	currentAnomaly = anomalies.pick_random()
 
-	if world_environment:
+	if sunRef:
 		if currentAnomaly.is_in_group("SunChanger"):
-			call_deferred("_apply_sun_material", SUN_EYED)
+			sunRef.texture = SUN_EYED
+			sunRef.scale = Vector3(22,22,22)
 	else:
 		currentAnomaly.visible = true
 
@@ -25,12 +26,9 @@ func ResolveAnomaly():
 	super.ResolveAnomaly()
 
 	if currentAnomaly.is_in_group("SunChanger"):
-		call_deferred("_apply_sun_material", SUN)
+		sunRef.texture = SUN
+		sunRef.scale = Vector3(4,4,4)
 	else:
 		currentAnomaly.visible = false
 
 	currentAnomaly = null
-
-func _apply_sun_material(mat):
-	if world_environment and world_environment.environment and world_environment.environment.sky:
-		world_environment.environment.sky.sky_material = mat
